6c58a26a98d7efc9a690a088c714942f6455c5fa
Szilvia Hosser-Cox <szilvia.hosser@gmail.com>
2018-08-31 20:03:15 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: { minimum: 100 }
-  validates :category, inclusion: { }
+  validates :category, inclusion: { in: %w(Fisction Non-Fiction)}
 end
