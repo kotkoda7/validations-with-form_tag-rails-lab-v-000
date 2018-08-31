cb44a007417737c86e1017d46a00e2ff7e5cac17
Szilvia Hosser-Cox <szilvia.hosser@gmail.com>
2018-08-31 20:02:53 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: { minimum: 100 }
-  validates :category, inclusion
+  validates :category, inclusion: { }
 end
